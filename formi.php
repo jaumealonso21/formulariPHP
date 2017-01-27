6cd2e05d71ff2a5d061cce600269109777f71b30
jaumealonso21 <jaumealonso@hotmail.com>
2017-01-27 16:53:57 +0100

    Versió  1.5 Amb calendari, falta gènere

--- formi.php
+++ formi.php
@@ -1,6 +1,7 @@
 <?php
+
 $enviar = filter_input(0, 'submit');
-$errorNom = ""; $errorApellidos = ""; $errorNick = ""; $errorDate = "";
+$errorNom = ""; $errorApellidos = ""; $errorNick = ""; $errorDate = ""; $errorGenero = "";
 //$nombre = filter_input(INPUT_POST, 'nombre');
 //$enviar = true; //True dóna per enviat el formulari
 if(isset($enviar)){
@@ -10,6 +11,7 @@ if(isset($enviar)){
     $apellidos = filter_input(INPUT_POST, 'apellidos', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $nick = filter_input(INPUT_POST, 'nick', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $date = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $genero = filter_input(INPUT_POST, 'genero');
     
     if($nombre === "") {
         $errorNom = $errors[0];
@@ -23,32 +25,48 @@ if(isset($enviar)){
     }
     if($nick === "") {
         $errorNick = $errors[0];
-    }if ($date === ""){
+    }
+    if ($date === ""){
         $errorDate = $errors[0];
     }else{
-        $dateSplit = explode("/", $date);
-        $verifDate = checkdate($dateSplit[1], $dateSplit[0], $dateSplit[2]);//month, day, year
+        //$dateSplit = explode("/", $date);
+        list($dia, $mes, $any) = explode("/", $date);
+        //$verifDate = checkdate($dateSplit[1], $dateSplit[0], $dateSplit[2]);
+        $verifDate = checkdate($mes, $dia, $any);//month, day, year
         if($verifDate){
-            $any_dif = date("Y") - $dateSplit[2];
-            $mes_dif = date("m") - $dateSplit[1];
-            $dia_dif = date("d") - $dateSplit[0];
-            if ($dia_dif < 0 || $mes_dif < 0) { 
-                $date = $any_dif--;
-                
-            }//Ajustament de l'any
-            if($any_dif<17){
-                $date = $errors[3];
+//            $any_dif = date("Y") - $dateSplit[2];
+//            $mes_dif = date("m") - $dateSplit[1];
+//            $dia_dif = date("d") - $dateSplit[0];
+
+            $any_dif = date("Y") - $any;
+            $mes_dif = date("m") - $mes;
+            $dia_dif = date("d") - $dia;
+            if($any_dif<18){
+                $errorDate = $errors[3];//Menor d'edat
+            }elseif ($any_dif===18) {//Comprovem mesos
+                if($mes_dif<0){
+                    $errorDate = $errors[3];//Menor d'edat
+                }elseif ($mes_dif===0){//Comprovem dies
+                    if($dia_dif<0){
+                        $errorDate = $errors[3];//Menor d'edat
+                    }
+                }
             }
+            
         }else{
-            $date = $errors[2];
+            $errorDate = $errors[2];//Data incorrecta
         }
+    }
+    if ($genero === ""){
+        $errorGenero = $errors[0];
     }
     
 }else{
     $nombre = "";
     $apellidos = "";
     $nick = "";
-    $date = "dd/mm/yyyy";
+    //$date = "dd/mm/yyyy";
+    $date = "";
 }
 
 
@@ -96,19 +114,20 @@ if(isset($enviar)){
        <tr>
             <td>
                 <label for="data">Data naixament <span class="req">(requerido)</span></label>
-    <input type="text" name="data" id="data" value="<?php echo $date ?>" />
-            </td>
-        </tr><!--
+                <input type="text" name="data" id="data" value="<?php echo $date ?>" placeholder="dd/mm/yyyy" />
+    <span><?php echo $errorDate; ?></span></td>
+        </tr>
         <tr>
             <td>
               <span>Gènere: </span>
     <label for="H"><span>H</span></label>
-    <input type="radio" name="H" id="H" value="male" checked>
+    <input type="radio" name="genero" id="H" value="hombre">
     <label for="D"><span>D</span></label>
-    <input type="radio" name="D" id="D" value="dona">  
+    <input type="radio" name="genero" id="D" value="mujer">
+    <span><?php echo $errorGenero; ?></span>
             </td>
         </tr>
-        <tr>
+<!--        <tr>
             <td>
                <label for="password">Password <span>(requerido)</span></label>
     <input type="password" name="password" id="password" /> 
